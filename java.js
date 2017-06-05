ae0796a0e31f1b9be48d84574d0c94cf3bd787b5
GabenStoleYourWallet <bailey.t.adams@gmail.com>
2017-06-05 14:33:34 -0700

    gravity?

--- java.js
+++ java.js
@@ -1,8 +1,15 @@
 var file = 100.0;
+var Vx = 5;
+var Vy = 0;
+var loc = createVector(100.0, 100.0);
+var vel = createVector(Vx, Vy);
+var g = createVector(0, .5);
 function setup() {
 	createCanvas(800, 500);
 }
 function draw() {
-	fill(0);
-	ellipse(file, file, file, file);
+	background(155);
+	fill(255);
+	loc.add(vel)add(g);
+	ellipse(loc.x, loc.y, file, file);
 }
